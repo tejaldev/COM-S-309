cc0baa07d4323885dc7753034aab49e09ea3b2e9
raghuram <rguddati@iastate.edu>
2023-09-06 12:28:24 -0500

    hello people from tutorials

--- Experiments/rguddati/Exp1/hellopeople/src/main/java/coms309/WelcomeController.java
+++ Experiments/rguddati/Exp1/hellopeople/src/main/java/coms309/WelcomeController.java
@@ -2,20 +2,18 @@ package coms309;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.bind.annotation.PathVariable;
+
+/**
+ * Simple Hello World Controller to display the string returned
+ *
+ * @author Vivek Bengre
+ */
 
 @RestController
 class WelcomeController {
 
     @GetMapping("/")
     public String welcome() {
-
         return "Hello and welcome to COMS 309";
-    }
-
-    @GetMapping("/{name}")
-    public String welcome(@PathVariable String name) {
-
-        return "Hello and welcome to COMS 309: " + name;
     }
 }
