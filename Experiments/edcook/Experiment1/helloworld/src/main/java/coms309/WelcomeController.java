8d34d0f014c1c7b210c765ed8443a634781f30ce
Ella Cook <ellacook@edcook.student.iastate.edu>
2023-09-06 18:49:25 -0500

    changed WelcomeController message

--- Experiments/edcook/Experiment1/helloworld/src/main/java/coms309/WelcomeController.java
+++ Experiments/edcook/Experiment1/helloworld/src/main/java/coms309/WelcomeController.java
@@ -9,11 +9,11 @@ class WelcomeController {
 
     @GetMapping("/")
     public String welcome() {
-        return "Hello and welcome to COMS 309";
+        return "Hello and welcome to COMS 309 ip_107 GlobeGather!";
     }
 
     @GetMapping("/{name}")
     public String welcome(@PathVariable String name) {
-        return "Hello and welcome to COMS 309: " + name;
+        return "Hello and welcome to COMS 309 ip_107 GlobeGather!): " + name;
     }
 }
