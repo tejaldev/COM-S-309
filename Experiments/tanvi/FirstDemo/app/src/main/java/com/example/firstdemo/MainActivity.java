185840fb0513020541575afd163c9a6316b90343
Tanvi <tanvi.mehetre03@gmail.com>
2023-09-08 23:28:26 -0500

    login commit

--- Experiments/tanvi/FirstDemo/app/src/main/java/com/example/firstdemo/MainActivity.java
+++ Experiments/tanvi/FirstDemo/app/src/main/java/com/example/firstdemo/MainActivity.java
@@ -3,12 +3,38 @@ package com.example.firstdemo;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
+    EditText Username;
+    EditText Password;
+    Button loginButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Username = findViewById(R.id.Username);
+        Password = findViewById(R.id.Password);
+        loginButton = findViewById(R.id.loginButton);
+
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(Username.getText().toString().equals("user") && Password.getText().toString().equals("1234"))
+                {
+                    Toast.makeText(MainActivity.this, "Successfully Logged In", Toast.LENGTH_SHORT).show();
+                }
+                else
+                {
+                    Toast.makeText(MainActivity.this, "Failed Log in", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
     }
 }
