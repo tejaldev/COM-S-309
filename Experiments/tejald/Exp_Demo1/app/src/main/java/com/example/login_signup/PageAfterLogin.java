b0d60654495a89f918eb36e4e7023a81c43ecdb1
tejal <tejal@iastate.edu>
2023-09-12 12:52:41 -0500

    working demo1

---
+++ Experiments/tejald/Exp_Demo1/app/src/main/java/com/example/login_signup/PageAfterLogin.java
@@ -0,0 +1,35 @@
+package com.example.login_signup;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+
+public class PageAfterLogin extends AppCompatActivity {
+
+    Button returnBack;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_page_after_login2);
+
+        returnBack=findViewById(R.id.buttonToReturnBack);
+        returnBack.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                returnBackHomePage();
+            }
+        });
+
+    }
+
+    public void returnBackHomePage(){
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+    }
+}
