28dcb8631b05e78da2698f8ac1cc8d1b301cd0ac
raghuram <rguddati@iastate.edu>
2023-10-06 10:43:32 -0500

    deleting unrequired files

--- Backend/manytomany/tutorial/src/main/java/com/cs309/tutorial/course/CourseRepository.java
+++
@@ -1,9 +0,0 @@
-package com.cs309.tutorial.course;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface CourseRepository extends JpaRepository<Course, Long>{
-
-}
--- Backend/manytomany/tutorial/src/main/java/com/cs309/tutorial/post/PostRepository.java
+++
@@ -1,9 +0,0 @@
-package com.cs309.tutorial.post;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface PostRepository extends JpaRepository<Post, Long> {
-
-}
