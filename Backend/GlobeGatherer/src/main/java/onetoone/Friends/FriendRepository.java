9b3427d92725007bb4aee519ad6344f32e66a769
raghuram <rguddati@iastate.edu>
2023-10-07 18:08:21 -0500

    renaming files to reduce merge conflicts

---
+++ Backend/GlobeGatherer/src/main/java/onetoone/Friends/FriendRepository.java
@@ -0,0 +1,17 @@
+package onetoone.Friends;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.transaction.annotation.Transactional;
+
+/**
+ * 
+ * @author Vivek Bengre
+ * 
+ */ 
+
+public interface FriendRepository extends JpaRepository<Friend, Long> {
+    Friend findById(int id);
+
+    @Transactional
+    void deleteById(int id);
+}
