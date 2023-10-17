13d043b1d1ecd82cd573ef1885e6b80415adddae
Tanvi <tanvi.mehetre03@gmail.com>
2023-10-17 10:44:47 -0500

    Merging

---
+++ Frontend/app/src/main/java/com/example/globegatherer/Activity2_login_next.java
@@ -0,0 +1,85 @@
+package com.example.globegatherer;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.example.globegatherer.R;
+
+import org.json.JSONObject;
+
+public class Activity2_login_next extends AppCompatActivity {
+
+    private Button signUpButton;
+    private EditText signUpNameEditText, signUpUsernameEditText, signUpPasswordEditText, signUpEmailEditText, signUpPhoneNoEditText;
+    private TextView responseTextView;
+    private NetworkManager networkManager;
+
+    private static final String URL_JSON_OBJECT = "http://coms-309-013.class.las.iastate.edu:8080/persons/add";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity2_login_next);
+
+        signUpButton = findViewById(R.id.SignUpButton);
+        signUpNameEditText = findViewById(R.id.SignUpName);
+        signUpUsernameEditText = findViewById(R.id.SignUpUsername);
+        signUpPasswordEditText = findViewById(R.id.SignUpPassword);
+        signUpEmailEditText = findViewById(R.id.SignUpEmail);
+        signUpPhoneNoEditText = findViewById(R.id.SignUpPhoneNo);
+        responseTextView = findViewById(R.id.Response);
+
+        networkManager = NetworkManager.getInstance(this);
+
+        signUpButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                makeJsonObjReq();
+            }
+        });
+    }
+
+    private void makeJsonObjReq() {
+        // Create a JSON object with the user's input data
+        JSONObject requestData = new JSONObject();
+        try {
+            requestData.put("signUpName", signUpNameEditText.getText().toString());
+            requestData.put("signUpUsername", signUpUsernameEditText.getText().toString());
+            requestData.put("signUpPassword", signUpPasswordEditText.getText().toString());
+            requestData.put("signUpEmail", signUpEmailEditText.getText().toString());
+            requestData.put("signUpPhoneNo", signUpPhoneNoEditText.getText().toString());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        networkManager.sendPostRequest(
+                requestData,
+                URL_JSON_OBJECT,
+                new Response.Listener<JSONObject>() {
+                    @Override
+                    public void onResponse(JSONObject response) {
+                        try {
+                            Log.d("Volley Response", response.toString());
+                            responseTextView.setText(response.toString());
+                        } catch (Exception e) {
+                            e.printStackTrace();
+                        }
+                    }
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        Log.e("Volley Error", error.toString());
+                    }
+                }
+        );
+    }
+}
---
+++ Frontend/app/src/main/java/com/example/globegatherer/NetworkManager.java
@@ -0,0 +1,64 @@
+package com.example.globegatherer;
+
+import android.content.Context;
+
+import com.android.volley.DefaultRetryPolicy;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.toolbox.JsonArrayRequest;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+public class NetworkManager {
+    private static NetworkManager instance = null;
+    private RequestQueue requestQueue;
+    private static Context ctx;
+    private static final int MY_SOCKET_TIMEOUT_MS = 10000;
+    private NetworkManager(Context context) {
+        ctx = context.getApplicationContext();
+        requestQueue = getRequestQueue();
+    }
+
+    public static synchronized NetworkManager getInstance(Context context) {
+        if (instance == null) {
+            instance = new NetworkManager(context);
+        }
+        return instance;
+    }
+
+    public RequestQueue getRequestQueue() {
+        if (requestQueue == null) {
+            requestQueue = Volley.newRequestQueue(ctx);
+        }
+        return requestQueue;
+    }
+
+    public void sendPostRequest(JSONObject postData, String url, Response.Listener<JSONObject> responseListener, Response.ErrorListener errorListener)
+    {
+        JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(Request.Method.POST, url, postData, responseListener, errorListener);
+        getRequestQueue().add(jsonObjectRequest);
+    }
+
+    public void sendGetRequest(String url, Response.Listener<JSONArray> responseListener, Response.ErrorListener errorListener) {
+        JsonArrayRequest jsonArrayRequest = new JsonArrayRequest(Request.Method.GET, url, null, responseListener, errorListener);
+        getRequestQueue().add(jsonArrayRequest);
+    }
+
+
+    public void sendJsonObjectRequest(int method, String url, JSONObject jsonRequest, Response.Listener<JSONObject> listener, Response.ErrorListener errorListener) {
+        JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(method, url, jsonRequest, listener, errorListener);
+        jsonObjectRequest.setRetryPolicy(new DefaultRetryPolicy(
+                MY_SOCKET_TIMEOUT_MS,
+                DefaultRetryPolicy.DEFAULT_MAX_RETRIES,
+                DefaultRetryPolicy.DEFAULT_BACKOFF_MULT));
+        requestQueue.add(jsonObjectRequest);
+    }
+
+
+
+
+}
