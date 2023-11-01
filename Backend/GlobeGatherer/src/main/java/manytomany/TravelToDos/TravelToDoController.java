378c7a87cb29b6067338d1a74d9e29babceda76b
Ella Cook <ellacook@edcook.student.iastate.edu>
2023-11-01 18:39:33 -0500

    added backend for google calendar

--- Backend/GlobeGatherer/src/main/java/manytomany/TravelToDos/TravelToDoController.java
+++ Backend/GlobeGatherer/src/main/java/manytomany/TravelToDos/TravelToDoController.java
@@ -1,4 +1,4 @@
-package manytomany.TravelHistories;
+package manytomany.TravelToDos;
 
 import java.util.List;
 
@@ -12,54 +12,56 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
- * 
+ *
  * @author Vivek Bengre
- * 
- */ 
+ *
+ */
 
 @RestController
-public class TravelHistoryController {
+public class TravelToDoController {
 
     @Autowired
-    TravelHistoryRepository travelHistoryRepository;
-    
+    TravelToDoRepository travelToDoRepository;
+
     private String success = "{\"message\":\"success\"}";
     private String failure = "{\"message\":\"failure\"}";
 
-    @GetMapping(path = "/TravelHistory")
-    List<TravelHistory> getAllTravelToDo(){
-        return travelHistoryRepository.findAll();
+
+
+    @GetMapping(path = "/TravelToDo")
+    List<TravelToDo> getAllTravelToDo(){
+        return travelToDoRepository.findAll();
     }
 
-    @GetMapping(path = "/TravelHistory/{id}")
-    TravelHistory getTravelToDoById(@PathVariable int id){
-        return travelHistoryRepository.findById(id);
+    @GetMapping(path = "/TravelToDo/{id}")
+    TravelToDo getTravelToDoById(@PathVariable int id){
+        return travelToDoRepository.findById(id);
     }
 
-    @PostMapping(path = "/TravelHistory")
-    TravelHistory createTravelToDo(@RequestBody TravelHistory travel){
+    @PostMapping(path = "/TravelToDo")
+    TravelToDo createTravelToDo(@RequestBody TravelToDo travel){
         if (travel == null)
             return null;
-        return travelHistoryRepository.save(travel);
+        return travelToDoRepository.save(travel);
     }
 
 
-    @PutMapping(path = "/TravelHistory/{id}")
-    TravelHistory updateTravelToDo(@PathVariable int id, @RequestBody TravelHistory request){
-        TravelHistory travel = travelHistoryRepository.findById(id);
+    @PutMapping(path = "/TravelToDo/{id}")
+    TravelToDo updateTravelToDo(@PathVariable int id, @RequestBody TravelToDo request){
+        TravelToDo travel = travelToDoRepository.findById(id);
         if(travel == null)
             return null;
-        travelHistoryRepository.save(request);
-        return travelHistoryRepository.findById(id);
+        travelToDoRepository.save(request);
+        return travelToDoRepository.findById(id);
     }
 
-    @DeleteMapping(path = "/TravelHistory/{id}")
+    @DeleteMapping(path = "/TravelToDo/{id}")
     String deleteTravelToDo(@PathVariable int id){
 
         // Check if there is an object depending on user and then remove the dependenc
 
         // delete the laptop if the changes have not been reflected by the above statement
-        travelHistoryRepository.deleteById(id);
+        travelToDoRepository.deleteById(id);
         return success;
     }
 }
