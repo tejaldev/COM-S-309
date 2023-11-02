41ed9cdcd9b587c1e17d2635fe3493480c66b14f
Ella Cook <ellacook@edcook.student.iastate.edu>
2023-11-01 19:00:34 -0500

    moved websocket3 out of the way

--- Backend/websocket3/src/main/java/com/cs309/websocket3/notification/NotificationRepository.java
+++
@@ -1,7 +0,0 @@
-package com.cs309.websocket3.notification;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface NotificationRepository extends JpaRepository<Notification, Long>{
-
-}
