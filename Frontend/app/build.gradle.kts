64132d31ed9d61116118f8ae551f7491c1b0d0c8
tejal <tejal@iastate.edu>
2023-11-04 15:36:02 -0500

    Push before merge

--- Frontend/app/build.gradle.kts
+++ Frontend/app/build.gradle.kts
@@ -47,7 +47,12 @@ dependencies {
     implementation ("com.google.android.gms:play-services-maps:17.0.1")
 
     implementation ("com.google.android.gms:play-services-maps:17.0.0")
-    implementation ("com.google.android.gms:play-services-location:18.0.0")
+    implementation ("com.google.android.gms:play-services-location:17.0.0")
 
     // code ends here
+//
+//    implementation ("com.google.android.libraries.places:places:2.7.0")
+    implementation ("com.google.android.libraries.places:places:2.6.0")
+    implementation ("com.google.android.gms:play-services-places:17.0.0")
+
 }
